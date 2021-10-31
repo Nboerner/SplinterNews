085954debf9590517bacab4edea4d7ffd8e3f5f7
Nboerner <noahboerner@gmail.com>
2021-10-31 17:57:52 -0400

    Articles automatically load on scrolling to the end

--- app/src/main/java/com/example/woven_news/MainActivity.kt
+++ app/src/main/java/com/example/woven_news/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.woven_news.adapter.PanelAdapter
@@ -22,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 
     // adapter used by the RecyclerView to handle population of articles from HTTP request
     private lateinit var adapter : PanelAdapter
+
+    private lateinit var scrollListener : RecyclerView.OnScrollListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,6 +78,8 @@ class MainActivity : AppCompatActivity() {
             },
         )
 
+        setRecyclerScrollListener(recyclerView, viewModel)
+
 
 
 
@@ -101,6 +106,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+
+    private fun setRecyclerScrollListener(recycler : RecyclerView, viewModel : MainViewModel) {
+
+        scrollListener = object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val totalArticles = recyclerView.layoutManager!!.itemCount
+                if (totalArticles == linearLayoutManager.findLastVisibleItemPosition() + 1) {
+                    Log.d("RecyclerView", "Loading new Content on scroll")
+                    if (viewModel.activeStories == viewModel.recentStories) {
+                        Log.d("RecyclerView", "Loading new recent articles")
+                        viewModel.updateView(true)
+                    } else {
+                        Log.d("RecyclerView", "Loading new best articles")
+                        viewModel.updateView(false)
+                    }
+                    recyclerView.removeOnScrollListener(scrollListener)
+                    setRecyclerScrollListener(recyclerView, viewModel)
+                }
+            }
+        }
+        recycler.addOnScrollListener(scrollListener)
+        Log.d("RecyclerView", "Setting new listener")
+
+    }
 
     /**
      * Checks if the device is currently connected to an active internet connection
