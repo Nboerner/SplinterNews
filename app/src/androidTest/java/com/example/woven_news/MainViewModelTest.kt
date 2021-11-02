44d342e80cceea5dc810a11f636b94c7ce8cb586
Nboerner <noahboerner@gmail.com>
2021-11-02 12:57:28 -0400

    Attempted JUnit and Mockito Testing

---
+++ app/src/androidTest/java/com/example/woven_news/MainViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.example.woven_news
+
+//import android.content.Context
+//import com.android.dx.command.Main
+//import org.junit.*
+//import org.junit.runner.RunWith
+//import org.mockito.ArgumentMatchers
+//import org.mockito.Mock
+//import org.mockito.Mockito
+//import org.mockito.Mockito.`when`
+//import org.mockito.MockitoAnnotations
+//import org.mockito.junit.MockitoJUnitRunner
+
+//private const val FAKE_STRING = "Woven News"
+//
+//@RunWith(MockitoJUnitRunner::class)
+//class MainViewModelTest {
+//
+////    @get:Rule
+////    val instantTaskExecutorRule = InstantTaskExecutorRule()
+//
+////    @Mock
+////    private lateinit var mockContext : Context
+////
+////    lateinit var mainViewModel : MainViewModel
+////
+////    @Before
+////    fun initialize() {
+////        MockitoAnnotations.openMocks(this)
+////        this.mainViewModel = MainViewModel()
+////
+////    }
+////
+////
+////    @Test
+////    fun retrieveStoryIDS_positiveResponse() {
+////        // our Mock API response
+////        `when`(this.mainViewModel.init()).thenAnswer {
+////            return@thenAnswer ArgumentMatchers.anyList<String>()
+////        }
+////    }
+//
+//
+//}
---
+++ app/src/test/java/com/example/woven_news/networkTest.kt
@@ -0,0 +1,15 @@
+package com.example.woven_news
+
+//import android.content.Context
+//import android.net.wifi.WifiManager
+//import androidx.core.content.ContextCompat.getSystemService
+import org.junit.Test
+
+class NetworkTest {
+    @Test
+    fun isConnected() {
+//        val connectionManager = getSystemService(Context)
+//        connectionManager.isWifiEnabled(false)
+
+    }
+}
